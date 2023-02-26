177c6c4bbaa575e3403e560ad0f757b74a13093e
RezaSamani <111186776+RezaSamani@users.noreply.github.com>
2023-02-25 19:57:44 -0800

    final level 300

--- js.js
+++ js.js
@@ -5,7 +5,6 @@ function episodes() {
     fetch("https://api.tvmaze.com/shows/82/episodes")
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
         call(res);
       })
       .catch((err) => console.log(err));
@@ -14,49 +13,98 @@ function episodes() {
   // image name ep
   let content = document.querySelector(".content");
   let search=document.getElementById("site-search");
+  let selectt=document.getElementById("select");
 
   function call(res) {
     res.map((element) => {
       const card = document.createElement("div");
       card.classList.add("card");
       card.style.width = "18rem";
-      content.append(card);
+      
   
       // create Element content
-      const block=document.createElement("section");
       const forLink=document.createElement("a");
       const image = document.createElement("img");
-      const name = document.createElement("h3");
-      const number = document.createElement("h4");
+      const name = document.createElement("h4");
+      const number = document.createElement("h5");
       const epsummery = document.createElement("p");
+      const options=document.createElement("option");
       // end of Element content
-  
+
       // start content Element
-      block.classList.add("sect");
       image.src = element.image.medium;
       name.innerText = element.name;
-      forLink.href=element.url;
-      // delete <p></p> from summery
-      let str=element.summary;
-     str= str.slice(3, -5);
-      epsummery.innerText =str;
-      // chek Episode and season For this Format S01E04
-      element.number <= 9?
-        number.innerText = `S0${element.season}E0${element.number}`
-      :
-        number.innerText = `S0${element.season}E${element.number}`
-      //append
       
-      block.append(image, name, number,epsummery);
-      forLink.append(block)
-      content.append(forLink)
+    
+      forLink.href=element.url;
+      forLink.innerText="show more..."
+      // delete <p></p> from summery
+      epsummery.innerText =element.summary.slice(3, 60)+" . . . .".replaceAll("</p>","").replaceAll("<p>","");
+      // chek Episode and season For this Format S01E04
+      if(element.number <= 9){
+      
+        number.innerText = `S0${element.season}E0${element.number}`;
+        options.innerText=`S0${element.season}E0${element.number}-${element.name}`; 
+      }
+        else {
+        number.innerText = `S0${element.season}E${element.number}`
+        options.innerText=`S0${element.season}E${element.number}-${element.name}`;  
+      }
+        //append
+      selectt.append(options);
+      card.append(image, name, number,epsummery,forLink);
+      content.append(card);
     });
   }
+  
+  
 
-  search.addEventListener("onchange",(event)=>{
-    let val=event.target.value.toLowerCase();
-    const allEpisods=document.getElementsByClassName("sect");
-     });
-  // console.log(search);
-  // ("onchange",(ev)=>episodes())
-  // 
+  search.addEventListener("input",(event)=>{
+     let count=0; 
+     const searchText=event.target.value.toLowerCase();
+     const episodsCards=document.getElementsByClassName("card");
+     const result=document.getElementById("result")
+     
+
+       // iterat a values of carsd object 
+       for (let index = 0; index < episodsCards.length; index++) {
+       const theCard=episodsCards[index];
+       if(theCard.innerText.toLowerCase().includes(searchText))
+       {
+        count++;
+        theCard.classList.remove("disable")
+       }
+       else
+       {
+        theCard.classList.add("disable")
+       }
+      }
+      result.innerText=`${count} results`; 
+     
+  })
+  selectt.addEventListener("click",(event)=>{
+    let optionText=event.target.value.toLowerCase().slice(7);
+    const episodsCards=document.getElementsByClassName("card");
+    
+    if(optionText=="sods"){
+       optionText="";
+    }
+
+  
+      // iterat a values of carsd object 
+      for (let index = 0; index < episodsCards.length; index++) {
+      const theCard=episodsCards[index];
+      if(theCard.innerText.toLowerCase().includes(optionText))
+      {
+       
+       theCard.classList.remove("disable")
+      }
+      else
+      {
+       theCard.classList.add("disable")
+      }
+     }
+
+  })
+    //1 show allepisodes at first 
+    episodes();
